e9684ac54d33da8f40c91b01141902fa237f555c
Pranav Agrawal <pranavagrawal321@gmail.com>
2024-07-06 22:15:18 +0530

    Updated Voiceover

--- voiceover.py
+++ voiceover.py
@@ -1,14 +1,36 @@
 import pyttsx3
+import os
 
 voiceoverDir = "Voiceovers"
 
 
-def create_voice_over(fileName, text):
-    filePath = f"{voiceoverDir}/{fileName}.mp3"
-    engine = pyttsx3.init()
-    engine.save_to_file(text, filePath)
-    engine.runAndWait()
-    return filePath
+def create_voice_over(fileName, text, voice=None, rate=None, volume=None):
+    try:
+        os.makedirs(voiceoverDir, exist_ok=True)
+
+        filePath = os.path.join(voiceoverDir, f"{fileName}.mp3")
+
+        engine = pyttsx3.init()
+
+        if voice:
+            voices = engine.getProperty('voices')
+            engine.setProperty('voice', voices[voice].id)
+
+        if rate:
+            engine.setProperty('rate', rate)
+
+        if volume:
+            engine.setProperty('volume', volume)
+
+        engine.save_to_file(text, filePath)
+        engine.runAndWait()
+
+        return filePath
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
 
-create_voice_over("voice", "Hello World")
+if __name__ == '__main__':
+    create_voice_over('test_custom', 'This is a custom voiceover', voice=1, rate=150, volume=0.9)
